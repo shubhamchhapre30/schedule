d76e513699552bc9011eff614e46f0a68a5291a7
shubham <shubham.mobiwebtech@gmail.com>
2019-06-20 12:08:45 +0530

    add

--- Pipeline_aws_credentials.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p deploy/artifacts/keys
-echo """access_key_id: $AWS_ACCESS_KEY
-secret_access_key: $AWS_SECRET
-region: ap-southeast-2""" > deploy/artifacts/keys/aws.yml
-
-cat deploy/artifacts/keys/aws.yml
